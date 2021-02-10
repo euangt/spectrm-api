d5ccd006039eb543c6087f8aa85c41a0028d6322
euangt <euan.gillespie@outlook.com>
2021-02-10 15:25:59 +0000

    create and destroy endpoints added

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -6,23 +6,42 @@ class Api::V1::MessagesController < Api::V1::BaseController
 
   def show
     @message = Message.find(params[:id])
+    @message.retrieval_count += 1
   end
 
   def update
     if @message.update(message_params)
       render :show
     else
-      render json: { errors: @message.errors.full_messages }, status: :unprocessable_entity
+      render_error
     end
+  end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      render :show, status: :created
+    else
+      render_error
+    end
+  end
+
+  def destroy
+    @message.destroy
+    head :no_content
   end
 
   private 
 
   def message_params
-    params.require(:message).permit(:id, :content)
+    params.require(:message).permit(:content)
   end
 
   def set_message
     @message = Message.find(params[:id])
+  end
+
+  def render_error
+    render json: { errors: @message.errors.full_messages }, status: :unprocessable_entity
   end
 end
