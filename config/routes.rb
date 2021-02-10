f5e1a418a4c8ab8757997005c6fdc0320cc88159
euangt <euan.gillespie@outlook.com>
2021-02-10 13:59:49 +0000

    first endpoint added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :messages
+      resources :messages, only: [ :index ]
     end
   end
 end
