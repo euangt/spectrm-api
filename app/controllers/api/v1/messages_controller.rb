7945156697c07dbbc870e08a6b31e70e1edbb13b
euangt <euan.gillespie@outlook.com>
2021-02-10 15:19:28 +0000

    update method updated for errors

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -12,7 +12,7 @@ class Api::V1::MessagesController < Api::V1::BaseController
     if @message.update(message_params)
       render :show
     else
-      render_error
+      render json: { errors: @message.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
@@ -24,10 +24,5 @@ class Api::V1::MessagesController < Api::V1::BaseController
 
   def set_message
     @message = Message.find(params[:id])
-  end
-
-  def render_error
-    render json: { errors: @message.errors.full_messages },
-      status: :unprocessable_entity
   end
 end
