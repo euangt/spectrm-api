c98c11c96a74498669e02f18e54862d03d6a6979
euangt <euan.gillespie@outlook.com>
2021-02-11 22:54:57 +0000

    seeds updated

--- db/seeds.rb
+++
@@ -1,8 +0,0 @@
-require 'faker'
-
-10.times do 
-  Message.create (
-    content: Faker::Lorem.sentence(word_count: 3, supplemental: true, random_words_to_add: 4,
-    retrieval_count: rand(1..10)
-  )
-end
