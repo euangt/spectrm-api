7d53946b87175eebe424cc6666fffbed7d490b72
euangt <euan.gillespie@outlook.com>
2021-02-11 23:51:30 +0000

    updated migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_10_133452) do
+ActiveRecord::Schema.define(version: 2021_02_11_234801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 2021_02_10_133452) do
 
   create_table "messages", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.text "content"
-    t.integer "retrieval_count"
+    t.integer "retrieval_count", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
