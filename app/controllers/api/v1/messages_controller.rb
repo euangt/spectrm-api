bc35321bb8744e8eb959e6e640c7d80e8cb510e4
euangt <euan.gillespie@outlook.com>
2021-02-11 22:17:09 +0000

    methods updated

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,11 +1,10 @@
 class Api::V1::MessagesController < Api::V1::BaseController
-  before_action :set_message, only: [ :show ]
+  before_action :set_message, only: [ :show, :update, :destroy ]
   def index
     @messages = Message.all
   end
 
   def show
-    @message = Message.find(params[:id])
     @message.retrieval_count += 1
   end
 
